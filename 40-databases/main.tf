d500e1bb78ff4859a743f8560d245ced45244ad8
unknown <eshwarkodavali123@gmail.com>
2025-10-29 19:13:39 +0530

    terraform11

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -37,131 +37,131 @@ provisioner "remote-exec" {
 }
 }
 
-#redis
+# #redis
 
-resource "aws_instance" "redis" {
-    ami           = local.ami_id
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [local.redis_sg_id]
-    subnet_id = local.database_subnet_id
-    tags = merge(
-        local.common_tags,
-        {
-            Name="${local.common_name_suffix}-redis"
-        }
+# resource "aws_instance" "redis" {
+#     ami           = local.ami_id
+#     instance_type = "t3.micro"
+#     vpc_security_group_ids = [local.redis_sg_id]
+#     subnet_id = local.database_subnet_id
+#     tags = merge(
+#         local.common_tags,
+#         {
+#             Name="${local.common_name_suffix}-redis"
+#         }
     
-    )
-}
+#     )
+# }
 
-resource "terraform_data" "redis" {
-  triggers_replace = [
-    aws_instance.redis.id # This trigger ensures the script runs every time Terraform applies
-  ]
+# resource "terraform_data" "redis" {
+#   triggers_replace = [
+#     aws_instance.redis.id # This trigger ensures the script runs every time Terraform applies
+#   ]
 
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = aws_instance.redis.private_ip
-  }
-     # terraform copies this file to mongodb server
-      provisioner "file" {
-        source      = "bootstrap.sh" 
-        destination = "/tmp/bootstrap.sh"
-      }
+#   connection {
+#     type     = "ssh"
+#     user     = "ec2-user"
+#     password = "DevOps321"
+#     host     = aws_instance.redis.private_ip
+#   }
+#      # terraform copies this file to mongodb server
+#       provisioner "file" {
+#         source      = "bootstrap.sh" 
+#         destination = "/tmp/bootstrap.sh"
+#       }
 
-provisioner "remote-exec" {
-  inline = [
-    "chmod +x /tmp/bootstrap.sh",
-    "sudo sh /tmp/bootstrap.sh redis"
-  ]
-}
-}
+# provisioner "remote-exec" {
+#   inline = [
+#     "chmod +x /tmp/bootstrap.sh",
+#     "sudo sh /tmp/bootstrap.sh redis"
+#   ]
+# }
+# }
 
-#rabbitmq
+# #rabbitmq
 
-resource "aws_instance" "rabbitmq" {
-    ami           = local.ami_id
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [local.rabbitmq_sg_id]
-    subnet_id = local.database_subnet_id
-    tags = merge(
-        local.common_tags,
-        {
-            Name="${local.common_name_suffix}-rabbitmq"
-        }
+# resource "aws_instance" "rabbitmq" {
+#     ami           = local.ami_id
+#     instance_type = "t3.micro"
+#     vpc_security_group_ids = [local.rabbitmq_sg_id]
+#     subnet_id = local.database_subnet_id
+#     tags = merge(
+#         local.common_tags,
+#         {
+#             Name="${local.common_name_suffix}-rabbitmq"
+#         }
     
-    )
-}
+#     )
+# }
 
-resource "terraform_data" "rabbitmq" {
-  triggers_replace = [
-    aws_instance.rabbitmq.id # This trigger ensures the script runs every time Terraform applies
-  ]
+# resource "terraform_data" "rabbitmq" {
+#   triggers_replace = [
+#     aws_instance.rabbitmq.id # This trigger ensures the script runs every time Terraform applies
+#   ]
 
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = aws_instance.rabbitmq.private_ip
-  }
-     # terraform copies this file to mongodb server
-      provisioner "file" {
-        source      = "bootstrap.sh" 
-        destination = "/tmp/bootstrap.sh"
-      }
+#   connection {
+#     type     = "ssh"
+#     user     = "ec2-user"
+#     password = "DevOps321"
+#     host     = aws_instance.rabbitmq.private_ip
+#   }
+#      # terraform copies this file to mongodb server
+#       provisioner "file" {
+#         source      = "bootstrap.sh" 
+#         destination = "/tmp/bootstrap.sh"
+#       }
 
-provisioner "remote-exec" {
-  inline = [
-    "chmod +x /tmp/bootstrap.sh",
-    "sudo sh /tmp/bootstrap.sh rabbitmq"
-  ]
-}
-}
+# provisioner "remote-exec" {
+#   inline = [
+#     "chmod +x /tmp/bootstrap.sh",
+#     "sudo sh /tmp/bootstrap.sh rabbitmq"
+#   ]
+# }
+# }
 
-#mysql
+# #mysql
 
-resource "aws_instance" "mysql" {
-    ami           = local.ami_id
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [local.mysql_sg_id]
-    subnet_id = local.database_subnet_id
-    iam_instance_profile = aws_iam_instance_profile.mysql.name
-    tags = merge(
-        local.common_tags,
-        {
-            Name="${local.common_name_suffix}-mysql"
-        }
+# resource "aws_instance" "mysql" {
+#     ami           = local.ami_id
+#     instance_type = "t3.micro"
+#     vpc_security_group_ids = [local.mysql_sg_id]
+#     subnet_id = local.database_subnet_id
+#     iam_instance_profile = aws_iam_instance_profile.mysql.name
+#     tags = merge(
+#         local.common_tags,
+#         {
+#             Name="${local.common_name_suffix}-mysql"
+#         }
     
-    )
-}
+#     )
+# }
 
-resource "aws_iam_instance_profile" "mysql" {
-  name = "mysql"
-  role = "EC2SSMParameterRead"
-}
+# resource "aws_iam_instance_profile" "mysql" {
+#   name = "mysql"
+#   role = "EC2SSMParameterRead"
+# }
 
-resource "terraform_data" "mysql" {
-  triggers_replace = [
-    aws_instance.mysql.id # This trigger ensures the script runs every time Terraform applies
-  ]
+# resource "terraform_data" "mysql" {
+#   triggers_replace = [
+#     aws_instance.mysql.id # This trigger ensures the script runs every time Terraform applies
+#   ]
 
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = aws_instance.mysql.private_ip
-  }
-     # terraform copies this file to mongodb server
-      provisioner "file" {
-        source      = "bootstrap.sh" 
-        destination = "/tmp/bootstrap.sh"
-      }
+#   connection {
+#     type     = "ssh"
+#     user     = "ec2-user"
+#     password = "DevOps321"
+#     host     = aws_instance.mysql.private_ip
+#   }
+#      # terraform copies this file to mongodb server
+#       provisioner "file" {
+#         source      = "bootstrap.sh" 
+#         destination = "/tmp/bootstrap.sh"
+#       }
 
-provisioner "remote-exec" {
-  inline = [
-    "chmod +x /tmp/bootstrap.sh",
-    "sudo sh /tmp/bootstrap.sh mysql dev"
-  ]
-}
-}
+# provisioner "remote-exec" {
+#   inline = [
+#     "chmod +x /tmp/bootstrap.sh",
+#     "sudo sh /tmp/bootstrap.sh mysql dev"
+#   ]
+# }
+# }
